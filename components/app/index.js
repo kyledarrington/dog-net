2a759b6a7a7ddd80d162704e6521ccaae7423f4e
kyledarrington <kyle@kyledarrington.com>
2019-09-23 22:19:45 -0400

    Add Navbar to App container

--- components/app/index.js
+++ components/app/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import NavBar from '../navbar'
 
 class App extends React.Component{
     render(){
         return (
             <div>
+                <NavBar />
                 <h1>
                     Hello World
                 </h1>
