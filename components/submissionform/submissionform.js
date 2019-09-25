9d3f4eda0d96e6137feed9013dc1113ec38e9e56
kyledarrington <kyle@kyledarrington.com>
2019-09-24 21:58:38 -0400

    New component - Submission Form
    Will allow user to submit a new post with image and text.

---
+++ components/submissionform/submissionform.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+
+class SubmissionForm extends React.Component{
+    render(){
+        return null
+    }
+}
+
+export default SubmissionForm
