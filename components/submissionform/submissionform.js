a9ddedd12e2992b84c068c9651c0a95796aa3f7e
kyledarrington <kyle@kyledarrington.com>
2019-09-25 13:16:18 -0400

    Build bare bones Formik render function

--- components/submissionform/submissionform.js
+++ components/submissionform/submissionform.js
@@ -2,8 +2,18 @@ import React from 'react'
 import { Formik, Form, Field, ErrorMessage } from 'formik'
 
 class SubmissionForm extends React.Component{
+    renderForm({errors, status, touched, isSubmitting}){
+         return (
+            <Form>
+                <Field component="textarea" name="text"></Field>
+                <ErrorMessage name="name" component="div" />
+            </Form>
+        )
+    }
     render(){
-        return null
+        return (
+            <Formik render={this.renderForm} />
+        )
     }
 }
 
