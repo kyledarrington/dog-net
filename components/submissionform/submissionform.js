291ccce525488da4ada62fd357e7e9bf8a9a5c79
kyledarrington <kyle@kyledarrington.com>
2019-09-26 13:11:01 -0400

    Getting rid of Formik May return to Formik later on, but for now, the constant re-rendering of all children of the Formik component any time a field is changed was causing issues. Namely, the thumbnail presented after uploading an image would re-render any time the textarea changed or blurred.

--- components/submissionform/submissionform.js
+++ components/submissionform/submissionform.js
@@ -1,32 +1,30 @@
 import React from 'react'
-import { Formik, Form, Field, ErrorMessage } from 'formik'
 import SubmissionThumb from './submissionthumb'
 import './style.scss'
 
 class SubmissionForm extends React.Component{
-    renderForm({values, setFieldValue}){
-         return (
-            <Form>
-                <Field component="textarea" rows="4" id="submission-text" name="submission-text"></Field>
-                <ErrorMessage name="submission-text" component="div" />
-                <div id="submission-img-container">
-                    <div id="img-input-container">
-                        <label for="image" id="img-input-wrapper">
-                            Choose File
-                            <input id="submission-img" name="image" type="file" onChange={(event) => {
-                                     setFieldValue("image", event.currentTarget.files[0])
-                                 }}/>
-                         </label>
-                    </div>
-                    <SubmissionThumb file={values.image} />
-                 </div>
-            </Form>
-        )
+    constructor(props){
+        super(props)
+        this.state={image: undefined}
     }
     render(){
         return (
             <div id="submission-form-container">
-                <Formik render={this.renderForm} />
+                <form>
+                    <div><textarea rows="4" id="submission-text" name="submission-text"></textarea></div>
+                    <div id="submission-img-container">
+                        <div id="img-input-container">
+                            <label htmlFor="image" id="img-input-wrapper">
+                                Choose File
+                                <input id="submission-img" name="image" type="file" onChange={(event) => {
+                                         this.setState({image: event.currentTarget.files[0]})
+                                     }}/>
+                             </label>
+                        </div>
+                        <SubmissionThumb file={this.state.image} />
+                     </div>
+                </form>
+               
             </div>
         )
     }
