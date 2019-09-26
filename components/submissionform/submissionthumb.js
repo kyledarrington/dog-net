386d19dac4b9712fae857918f32ebdaa7d9b722e
kyledarrington <kyle@kyledarrington.com>
2019-09-25 22:25:55 -0400

    New child component - SubmissionThumb Utilizes FileReader class to render a thumbnail of an uploaded image.

---
+++ components/submissionform/submissionthumb.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+class SubmissionThumb extends React.Component{
+    constructor(props){
+        super(props)
+        this.state={
+            loading:false,
+            imgSrc: undefined
+        }
+    }
+    
+    componentWillReceiveProps(newProps){
+        if (!newProps.file){return}
+        
+        this.setState({loading:true}, () => {
+            let reader = new FileReader()
+            
+            reader.onloadend = () => {
+                this.setState({loading:false, imgSrc: reader.result})
+            }
+            reader.readAsDataURL(newProps.file)
+        })
+    }
+    
+    render(){
+        if (!this.props.file){return null}
+        else if (this.state.loading){
+            return <div><p>loading...</p></div>
+        }
+        else{
+            return <div id="submission-thumb-container"><img src={this.state.imgSrc} alt={this.props.file.name} /></div>
+        }
+    }
+}
+    
+export default SubmissionThumb
