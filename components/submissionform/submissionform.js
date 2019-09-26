43f54132c1dcde6c3a1204bd3de98adccef480b0
kyledarrington <kyle@kyledarrington.com>
2019-09-25 22:28:00 -0400

    Flesh out Formik form. Add names/ids to fields. Add file input for image upload. Include new SubmissionThumb component. I see a good deal of re-naming in my future.

--- components/submissionform/submissionform.js
+++ components/submissionform/submissionform.js
@@ -1,18 +1,33 @@
 import React from 'react'
 import { Formik, Form, Field, ErrorMessage } from 'formik'
+import SubmissionThumb from './submissionthumb'
+import './style.scss'
 
 class SubmissionForm extends React.Component{
-    renderForm({errors, status, touched, isSubmitting}){
+    renderForm({values, setFieldValue}){
          return (
             <Form>
-                <Field component="textarea" name="text"></Field>
-                <ErrorMessage name="name" component="div" />
+                <Field component="textarea" rows="4" id="submission-text" name="submission-text"></Field>
+                <ErrorMessage name="submission-text" component="div" />
+                <div id="submission-img-container">
+                    <div id="img-input-container">
+                        <label for="image" id="img-input-wrapper">
+                            Choose File
+                            <input id="submission-img" name="image" type="file" onChange={(event) => {
+                                     setFieldValue("image", event.currentTarget.files[0])
+                                 }}/>
+                         </label>
+                    </div>
+                    <SubmissionThumb file={values.image} />
+                 </div>
             </Form>
         )
     }
     render(){
         return (
-            <Formik render={this.renderForm} />
+            <div id="submission-form-container">
+                <Formik render={this.renderForm} />
+            </div>
         )
     }
 }
