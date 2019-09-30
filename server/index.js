b7992f64aeacd31319996fc2ce01cac1019b387e
kyledarrington <kyle@kyledarrington.com>
2019-09-29 22:58:02 -0400

    Wrap knex migrate and seed calls in try/catch

--- server/index.js
+++ server/index.js
@@ -2,14 +2,19 @@ const express = require('express')
 const app = express()
 const port = 8080
 const environment = process.env.ENVIRONMENT || 'development'
-console.log(environment)
 const knexConfig = require('../knexfile.js')[environment]
 
 const knex = require('knex')(knexConfig)
 
 var knexInit = async () => {
-    await knex.migrate.latest()
-    await knex.seed.run()
+    try{
+        await knex.migrate.latest()
+        await knex.seed.run()
+
+    }
+    catch(err){
+        console.error(err);
+    }
 }
 
 knexInit()
