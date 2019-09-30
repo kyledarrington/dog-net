bd9d38cf50bdde1fe73059fb5840f476b6b0546e
kyledarrington <kyle@kyledarrington.com>
2019-09-29 22:53:23 -0400

    Update Users Seed File; Change seed order, add encryption Also includes some small syntax fixes

---
+++ database/seeds/02_users.js
@@ -0,0 +1,59 @@
+const config = require('../seed_config')
+const faker = require('faker')
+const bcrypt = require('bcrypt')
+
+exports.seed = async (knex) => {
+  // Deletes ALL existing entries
+  try{
+      await knex('users').del()
+      let rows = [],          
+          i = 1;
+      
+      var portraits  = await knex.select("id", "user_id").from('photos').where("type", "=", "portrait")
+      
+      let portraitMap = {}
+      for (var p of portraits) portraitMap[p.user_id] = p.id
+      console.log(portraitMap)
+      
+      //Generate 5 Dummy Users
+      while (i <= config.numberOf.users){
+          let portraitId = portraitMap[i]
+          rows.push(await newDummyUser(i, portraitId))
+          i++
+      }
+      
+      //Generate one Dev User with simple credentials
+      rows.push(await newDevUser('dev@dognet.dev', 'dognet'))
+      console.log(rows)
+      
+      //Insert generated users. 
+      await knex('users').insert(rows)
+  }
+  catch(err){
+      console.error(err)
+  }
+};
+
+const newDummyUser = async (userId, portraitId) => {
+    let password = faker.internet.password()
+    let hash = await bcrypt.hash(password, 10)
+    return{
+          id: userId,
+          first_name: faker.name.firstName(),
+          last_name: faker.name.lastName(),
+          email: 'dummy' + '+' + userId + '@dognet.dev',
+          portrait_id: portraitId,
+          password_hash: hash
+    }
+}
+
+const newDevUser = async (email, password) => {
+    let hash = await bcrypt.hash(email, 10);
+    return {
+        id: 0,
+        first_name: 'Dev',
+        last_name: 'User',
+        email: 'devuser@dognet.dev',
+        password_hash: await bcrypt.hash('dognet', 10)
+    }
+}
