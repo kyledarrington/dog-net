0280062594e589be98286018379bec44fe97408b
kyledarrington <kyle@kyledarrington.com>
2019-10-02 19:54:17 -0400

    Housekeeping: Remove init.js and rename user.js

--- store/user/index.js
+++ store/user/index.js
@@ -1,9 +1,9 @@
-function userToken(state = '', action){
+export default (state = '', action) => {
     switch(action.type){
         case 'SET' :
             return action.token
         case 'CLEAR' :
             return ''
     }
+    return state
 }
-
