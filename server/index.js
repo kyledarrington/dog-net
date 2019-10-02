527893d721a68918c397d8a64c5eab7f515fc6ce
kyledarrington <kyle@kyledarrington.com>
2019-10-02 13:10:38 -0400

    New Express Endpoint: 'login' Takes in an email and password, authenticates the login, and returns a JSON Web Token.

--- server/index.js
+++ server/index.js
@@ -3,16 +3,26 @@ const app = express()
 const port = 8080
 const environment = process.env.ENVIRONMENT || 'development'
 const knexConfig = require('../knexfile.js')[environment]
+const db = require('./local/database')
+const auth = require('.local/authentication')
 
 const knex = require('knex')(knexConfig)
 
 knexInit()
 
+app.use(express.json())
+
+app.post('/login', async (req, res) => {
+    const data = req.body,
+          user = await db.loginQuery(data.email),
+          token = (user != null) ? auth.getLoginToken(user, data.password) : null
+    return token
+}) 
+
 async function knexInit() {
     try{
         await knex.migrate.latest()
         await knex.seed.run()
-
     }
     catch(err){
         console.error(err);
