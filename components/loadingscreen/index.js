e532310106bfd6e0189417e5bafcf18e775ee039
kyledarrington <kyle@kyledarrington.com>
2019-10-06 17:00:03 -0400

    Implement redux-persist, redux-logger

---
+++ components/loadingscreen/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function LoadingScreen(props){
+    return (
+        <div><span>loading</span></div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,20 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {BrowserRouter as Router, Route} from 'react-router-dom'
-import App from '../components/app'
-import { createStore } from 'redux'
+
+import { applyMiddleware, createStore } from 'redux'
 import { Provider } from 'react-redux'
+import logger from 'redux-logger'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { PersistGate } from 'redux-persist/integration/react'
+
 import reducer from '../store/rootreducer'
+import LoadingScreen from '../components/loadingscreen'
+import App from '../components/app'
 
-const store = createStore(reducer)
-console.log(store.getState())
+const persistConfig = {
+ key: 'root',
+ storage,
+};
 
-
+const pReducer = persistReducer(persistConfig, reducer)
+const store = createStore(
+    pReducer,
+    applyMiddleware(logger)
+)
+const persistor = persistStore(store)
 
 ReactDOM.render((
     <Provider store={store}> 
-        <Router>
-            <App />
-        </Router>
+        <PersistGate loading={<LoadingScreen />} persistor={persistor}>
+            <Router>
+                <App />
+            </Router>
+        </PersistGate>
     </Provider>), 
     document.getElementById("root"))
