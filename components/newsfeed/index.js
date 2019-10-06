71a0220b5ddbe76f99c850b8987f430895b51f0c
kyledarrington <kyle@kyledarrington.com>
2019-10-06 17:12:13 -0400

    Newsfeed Component - Now queries post on render as well as on update (if necessary)

--- components/newsfeed/index.js
+++ components/newsfeed/index.js
@@ -12,9 +12,18 @@ class NewsfeedContainer extends React.Component{
         }
     }
     async componentDidMount(){
-        console.log('mounted');
         if (!this.props.user.token) return
-        let queriedPosts = (await axios.get('http://localhost:8081/feed?page=1&token=' + user.token)).data;
+        let queriedPosts = (await axios.get('http://localhost:8081/feed?page=1&token=' + this.props.user.token)).data;
+        this.getPosts(this.props.user.token)
+        return
+    }
+    async componentDidUpdate(prevProps){
+        if (!this.props.user.token || this.props.user.token == prevProps.user.token) return
+        this.getPosts(this.props.user.token)
+        return
+    }
+    async getPosts(token){
+        let queriedPosts = (await axios.get('http://localhost:8081/feed?page=1&token=' + token)).data;
         if (queriedPosts){
             this.setState({posts: queriedPosts})
         }
