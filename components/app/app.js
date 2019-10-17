fe90548b69263a74a85649edbeeb5f08182a7da9
kyledarrington <kyle@kyledarrington.com>
2019-10-16 21:57:18 -0400

    New Profile route. Also removed unused components

--- components/app/app.js
+++ components/app/app.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import Home from '../home'
-import NavBar from '../navbar'
 import Login from '../login'
-import NewsfeedContainer from '../newsfeed'
-import { Switch, Route, Redirect } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 import PrivateRoute from '../privateroute'
 import './style.scss'
+import ProfileContainer from '../profile'
 
 class App extends React.Component{
     render(){
         return(
             <div>
-                <Route path="/login" component={Login} />
-                <PrivateRoute path="/" component={Home} loggedIn={this.props.loggedIn} />
+                <Switch>
+                    <Route path="/login" component={Login} />
+                    <Route path="/profile/:userid" component={ProfileContainer} />
+                    <PrivateRoute path="/" component={Home} loggedIn={this.props.loggedIn} />
+                </Switch>
             </div>
         )
     }
