76f60a736e55da8948fb12929d0072db1cf42193
kyledarrington <kyle@kyledarrington.com>
2019-10-23 19:26:38 -0400

    Ammeding previous commit

--- components/profile/profile.js
+++ components/profile/profile.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react'
+import './style.scss'
 import PropTypes from 'prop-types'
 
-function Profile(props){
-    const user = props.user;
-    console.log(user)
-    return (
-        <div>
-            <h1> {user.first_name}'s Profile </h1>
-        </div>
-    )
+class Profile extends Component{
+    constructor(props){
+        super(props)
+    }
+    render(){
+        const user = this.props.user
+        return (
+            <div id="profile-container">
+                <div id="info-container">
+                    <div id="portrait">
+                        <img src={ user.portraitSrc } alt="" />
+                    </div>
+                    <div id="basic-info">
+                        <h3>{user.first_name + ' ' + user.last_name}</h3>
+                        <p>{user.email}</p>
+                    </div>
+                </div>
+                <div id="profile-feed">
+                    { this.props.feed }
+                </div>
+            </div>
+        )
+    }
+}
+
 Profile.propTypes = {
     onFollowClick : PropTypes.func,
     onUnfollowClick : PropTypes.func,
